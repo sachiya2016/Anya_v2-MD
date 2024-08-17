937827b1aefb76edce1e43e6bd0358575ac19808
sachiya2016 <sachinthalakshan919@gmail.com>
2024-08-17 14:21:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://sachinthalakshan919:pu00lOoNnH63lniA@cluster0.uuory.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    sessionId: process.env.SESSION_ID || "RFVJQW1KZ0QjZnViRm8zT_Queen-Anya_2tQMUZGRVpEU0pVT_Queen-Anya_E1qbFQ0OWd1Ukc4X3V4eGxaNm93LVI4cw==",
 
     /**
      * thumbnails and medias
